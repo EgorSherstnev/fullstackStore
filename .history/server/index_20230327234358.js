73575af9738b76a27fa06133e30779a02ff88d13
EgorSherstnev <svarog91@list.ru>
2023-03-27 23:54:11 +0300

    added sequelize

---
+++ .history/server/index_20230327234358.js
@@ -0,0 +1,11 @@
+require('dotenv').config()
+const express = require ('express');
+const sequelize = require('./db')
+
+const PORT = process.env.PORT || 5000
+
+const app = express()
+
+const start = async
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
---
+++ .history/server/index_20230327234420.js
@@ -0,0 +1,15 @@
+require('dotenv').config()
+const express = require ('express');
+const sequelize = require('./db')
+
+const PORT = process.env.PORT || 5000
+
+const app = express()
+
+const start = async () => {
+   try {
+      
+   }
+}
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
