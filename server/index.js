8d9155fe917c7203c676b4ce98ad9671899cb212
EgorSherstnev <svarog91@list.ru>
2023-03-29 23:48:36 +0300

    sequelize

--- server/index.js
+++ server/index.js
@@ -10,8 +10,8 @@ const app = express()
 
 const start = async () => {
    try {
-      //await sequelize.authenticate()
-      //await sequelize.sync()
+      await sequelize.authenticate()
+      await sequelize.sync()
       app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
    } catch (e) {
       console.log(e)
