7f72e1ff36e3db8e9f2c2aa0c223619496e7db27
EgorSherstnev <svarog91@list.ru>
2023-03-30 17:37:52 +0300

    added models.js

--- server/index.js
+++ server/index.js
@@ -10,23 +10,12 @@ const app = express()
 
 const start = async () => {
    try {
-      setTimeout(async () => {
-         try {
-            await sequelize.authenticate();
-            console.log('Connection has been established successfully.');
-            await sequelize.sync();
-            console.log('All models were synchronized successfully.');
-         } catch (error) {
-            console.error('Unable to connect to the database:', error);
-         }
-      }, 5000);
-      app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
+      await sequelize.authenticate()
+      await sequelize.sync()
+      app.listen(PORT, () => console.log(`Server started on ${PORT}`))
    } catch (e) {
       console.log(e)
    }
 }
 
 start()
-
-//app.use(express.json())
-//app.use('/test', testRouter);
