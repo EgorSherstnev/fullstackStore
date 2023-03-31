9f75dd57cba65e3beebd81ba893077ff1f4a9764
EgorSherstnev <svarog91@list.ru>
2023-04-01 01:52:34 +0300

    type controller

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ const cors = require('cors')
 const router = require('./routes/index')
 const errorHandler = require('./middleware/ErrorHandlingMiddleware')
 
-const testRouter = require('./routes/test.routes')
+//const testRouter = require('./routes/test.routes')
 
 const PORT = process.env.PORT || 5000
 
