1f3dec816ae4c777883035e1e063b170e86697d3
EgorSherstnev <svarog91@list.ru>
2023-04-12 22:51:38 +0300

    feat:add DeviceStore.js V1

---
+++ client/src/store/DeviceStore.js
@@ -0,0 +1,41 @@
+import {makeAutoObservable} from "mobx";
+
+export default class DeviceStore {
+   constructor() {
+      this._types = [
+         {id: 1, name: 'Холодильники'},
+         {id: 2, name: 'Смартфоны'}
+      ]
+      this._brands = [
+         {id: 1, name: "Samsung"},
+         {id: 2, name: "Apple"}
+      ]
+      this._devices = [
+         {id: 1, name: "Iphone 12 pro", price: 25000, rating: 5, img: `https://www.hdretail.ru/upload/iblock/8d0/8d01c02a7a68c45e7d021d9d44efab9f.jpg`},
+         {id: 1, name: "Iphone 12 pro", price: 25000, rating: 5, img: `https://www.hdretail.ru/upload/iblock/8d0/8d01c02a7a68c45e7d021d9d44efab9f.jpg`},
+         {id: 1, name: "Iphone 12 pro", price: 25000, rating: 5, img: `https://www.hdretail.ru/upload/iblock/8d0/8d01c02a7a68c45e7d021d9d44efab9f.jpg`},
+         {id: 1, name: "Iphone 12 pro", price: 25000, rating: 5, img: `https://www.hdretail.ru/upload/iblock/8d0/8d01c02a7a68c45e7d021d9d44efab9f.jpg`}
+      ]
+      makeAutoObservable(this)
+   }
+
+   setTypes(types) {
+      this._types = types
+   }
+   setBrands(brands) {
+      this._brands = brands
+   }
+   setDevices(devices) {
+      this._devices = devices
+   }
+
+   get types() {
+      return this._types
+   }
+   get brands() {
+      return this._brands
+   }
+   get devices() {
+      return this._devices
+   }
+}
