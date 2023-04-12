4f233cabe413fb5eb6a96d41fe439310673d400d
EgorSherstnev <svarog91@list.ru>
2023-04-13 01:03:32 +0300

    feat:add NavBar.js

--- client/src/index.js
+++ client/src/index.js
@@ -2,13 +2,15 @@ import React, { createContext } from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import UserStore from './store/UserStore';
+import DeviceStore from './store/DeviceStore';
 
 export const Context = createContext(null)
 
 ReactDOM.render(
   <React.StrictMode>
     <Context.Provider value={{
-      user: new UserStore()
+      user: new UserStore(),
+      device: new DeviceStore(),
     }}>
       <App />
     </Context.Provider>,
