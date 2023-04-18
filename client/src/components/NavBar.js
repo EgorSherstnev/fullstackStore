28d4a01d6cc769c8e32429315a094bf7a99cdea9
EgorSherstnev <svarog91@list.ru>
2023-04-19 00:34:07 +0300

    feat:add DevicePage.js

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -3,21 +3,34 @@ import { Context } from '../index';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import { SHOP_ROUTE } from '../utils/consts';
+import { ADMIN_ROUTE, LOGIN_ROUTE, SHOP_ROUTE } from '../utils/consts';
 import { NavLink } from 'react-router-dom';
 import Button from 'react-bootstrap/Button';
 import { observer } from 'mobx-react-lite';
+import { useNavigate } from 'react-router-dom';
 
 const NavBar = observer(() => {
    const {user} = useContext(Context)
+   const history = useNavigate()
    return (
       <Navbar bg="dark" variant="dark">
          <Container>
-            <NavLink style={{color:'white'}} to={SHOP_ROUTE}>КупиДевайс</NavLink>
+            <NavLink style={{color:'white'}} href={SHOP_ROUTE}>КупиДевайс</NavLink>
             {user.isAuth ?
                <Nav className="ml-auto" style={{color: 'white'}}>
-                     <Button variant={"outline-light"}>Админ панель</Button>
-                     <Button variant={"outline-light"} className='ml-2'>Войти</Button>
+                     <Button 
+                        variant={"outline-light"} 
+                        onClick={() => history(ADMIN_ROUTE)}
+                     >
+                        Админ панель
+                     </Button>
+                     <Button 
+                        variant={"outline-light"} 
+                        onClick={() => history(LOGIN_ROUTE)} 
+                        className='ml-2'
+                     >
+                        Выйти
+                     </Button>
                </Nav>
                :
                <Nav className="ml-auto" style={{color: 'white'}}>
