5c4615675ca4b660942c927ab0f27c5c1f6b018a
EgorSherstnev <svarog91@list.ru>
2023-04-26 00:15:02 +0300

    feat: up DeviceItem, deviceAPI, Shop

--- client/src/pages/Shop.js
+++ client/src/pages/Shop.js
@@ -1,10 +1,21 @@
-import React from "react";
+import React, { useContext, useEffect } from "react";
 import { Col, Container, Form } from "react-bootstrap";
 import TypeBar from "../components/TypeBar";
 import BrandBar from "../components/BrandBar";
 import DeviceList from "../components/DeviceList";
+import { observer } from "mobx-react-lite";
+import { Context } from "..";
+import { fetchBrands, fetchDevices, fetchTypes } from "../http/deviceAPI";
 
-const Shop = () => {
+const Shop = observer(() => {
+   const {device} = useContext(Context)
+
+   useEffect(() => {
+      fetchTypes().then(data => device.setTypes(data))
+      fetchBrands().then(data => device.setBrands(data))
+      fetchDevices().then(data => device.setDevices(data.rows))
+   },[])
+
    return (
       <Container>
          <Form className="mt-2">
@@ -18,6 +29,6 @@ const Shop = () => {
          </Form>
       </Container>
    )
-}
+})
 
 export default Shop;
--- client/src/store/DeviceStore.js
+++ client/src/store/DeviceStore.js
@@ -2,26 +2,9 @@ import {makeAutoObservable} from "mobx";
 
 export default class DeviceStore {
    constructor() {
-      this._types = [
-         {id: 1, name: 'Холодильники'},
-         {id: 2, name: 'Смартфоны'},
-         {id: 3, name: 'Ноутбуки'},
-         {id: 4, name: 'Телевизоры'}
-      ]
-      this._brands = [
-         {id: 1, name: "Samsung"},
-         {id: 2, name: "Apple"},
-         {id: 3, name: "Lenovo"},
-         {id: 4, name: "Asus"},
-      ]
-      this._devices = [
-         {id: 1, name: "Iphone 12 pro", price: 25000, rating: 5, img: `https://www.hdretail.ru/upload/iblock/8d0/8d01c02a7a68c45e7d021d9d44efab9f.jpg`},
-         {id: 2, name: "Iphone 12 pro", price: 25000, rating: 5, img: `https://www.hdretail.ru/upload/iblock/8d0/8d01c02a7a68c45e7d021d9d44efab9f.jpg`},
-         {id: 3, name: "Iphone 12 pro", price: 25000, rating: 5, img: `https://www.hdretail.ru/upload/iblock/8d0/8d01c02a7a68c45e7d021d9d44efab9f.jpg`},
-         {id: 4, name: "Iphone 12 pro", price: 25000, rating: 5, img: `https://www.hdretail.ru/upload/iblock/8d0/8d01c02a7a68c45e7d021d9d44efab9f.jpg`},
-         {id: 5, name: "Iphone 12 pro", price: 25000, rating: 5, img: `https://www.hdretail.ru/upload/iblock/8d0/8d01c02a7a68c45e7d021d9d44efab9f.jpg`},
-         {id: 6, name: "Iphone 12 pro", price: 25000, rating: 5, img: `https://www.hdretail.ru/upload/iblock/8d0/8d01c02a7a68c45e7d021d9d44efab9f.jpg`},
-      ]
+      this._types = []
+      this._brands = []
+      this._devices = []
       this._selectedType = {}
       this._selectedBrand = {}
       makeAutoObservable(this)
